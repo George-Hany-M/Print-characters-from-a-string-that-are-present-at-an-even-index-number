99f1357f6adce0b2411b8f32552485479c7af089
George Hany Milad <92086186+George-Hany-M@users.noreply.github.com>
2024-02-19 10:29:29 +0200

    code.py

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-# George Hany
+# My Name : George Hany
 # Write a program to accept a string from the user and display characters that are present at an even index number.
 
 # input the string from the user
